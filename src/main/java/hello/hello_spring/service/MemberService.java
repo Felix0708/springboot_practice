2514e9972c891d51f1a4897ffc254e70e2d992e9
Felix0708 <chuls0708@gmail.com>
2025-01-19 22:57:11 +0900

    회원 관리 예제 - 백엔드 개발

---
+++ src/main/java/hello/hello_spring/service/MemberService.java
@@ -0,0 +1,55 @@
+package hello.hello_spring.service;
+
+import hello.hello_spring.domain.Member;
+import hello.hello_spring.repository.MemberRepository;
+import hello.hello_spring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+    // cmd shift T 하면 쉽게 테스트 코드 생성 가능
+
+//    private  final MemberRepository memberRepository = new MemoryMemberRepository();
+    private  final MemberRepository memberRepository; // 테스트에서든 어디서든 같은 레포지토리를 사용하기 위해 이렇게 한다.
+    public MemberService(MemoryMemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    /**
+     * 회원가입
+     */
+    public Long join(Member member) {
+        // 같은 이름이 있는 중복 회원은 안됨
+//        Optional<Member> result = memberRepository.findByName(member.getName()); // opt cmd V 단축어 활용
+//        result.ifPresent(m -> {
+//            throw new IllegalStateException("이미 존재하는 회원입니다.");
+//        });
+//        memberRepository.findByName(member.getName()) // 전체 드래그 후에 ctrl T하면 리팩토링
+//                .ifPresent(m -> {
+//                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+//                });
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName()) // 전체 드래그 후에 ctrl T하면 리팩토링
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers() {
+        return  memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
