5c02c0dc4e1dbf64d3866cfd023f1972b0f52d4f
Felix0708 <chuls0708@gmail.com>
2025-01-21 21:45:39 +0900

    회원 관리 예제 - 웹 MVC 개발

---
+++ src/main/java/hello/hello_spring/controller/MemberForm.java
@@ -0,0 +1,16 @@
+package hello.hello_spring.controller;
+
+public class MemberForm {
+
+    private  String name;
+
+    // getter setter : cmd N
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
